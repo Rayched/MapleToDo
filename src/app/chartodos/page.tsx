486b2673c6351ed89c26353dfb3df32cdd4b7744
Rayched <ny2401@naver.com>
2026-08-12 15:28:18 +0900

    20260812 JSON undefined error 대응

--- src/app/chartodos/page.tsx
+++ src/app/chartodos/page.tsx
@@ -8,15 +8,12 @@ import { RequestCookies } from "next/dist/compiled/@edge-runtime/cookies";
 
 export default async function Chartodos_page(){
     const CookieStore = await cookies();
-    const Charnames = CookieStore.get("mapletodos_bookmarknames")
-
-    const Names: string[] = JSON.parse(decodeURIComponent(String(Charnames?.value)));
-    const GetAllSchedules = await GetBookmarksSchduleData(Names);
+    const Charnames = CookieStore.get("mapletodos_bookmarknames")?.value
 
     return (
         <div className={styles.chartodos_page_wrapper}>
             <BookmarkList 
-                AllScheduleData={GetAllSchedules}
+                
             />
         </div>
     );
